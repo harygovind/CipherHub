799724352a5d30ccb8e395dbccb23c5047eef416
Harigovind <121185937+harygovind@users.noreply.github.com>
2024-06-05 21:47:51 +0530

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -13,10 +13,10 @@ def main():
     choice = input("Enter your choice (1/2): ")
 
     if choice == '1':
-        passchecker_path = "C:\\Users\\hgpsm\\OneDrive\\Desktop\\Password\\passchecker.py"
+        passchecker_path = "C:\\Users\\Admin\\Desktop\\Password\\passchecker.py"
         subprocess.run(["python", passchecker_path])
     elif choice == '2':
-        passgenerator_path = "C:\\Users\\hgpsm\\OneDrive\\Desktop\\Password\\password_generator.py"
+        passgenerator_path = "C:\\Users\\Admin\\Desktop\\Password\\password_generator.py"
         subprocess.run(["python", passgenerator_path])
     else:
         print("Invalid choice. Please enter either 1 or 2.")
